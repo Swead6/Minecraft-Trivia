ab86189cc48b8ac41f6de6c673cb375bdaf492f7
Swead6 <54610515+Swead6@users.noreply.github.com>
2019-09-19 17:58:00 -0500

    Add files via upload

---
+++ index.hyperesources/triviaInfo.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"Minecraft Trivia";
+	window.questions = [		"WHAT IS THE NAME OF THE MALE DEFAULT SKIN?",
+								 "WHAT DO SKELETONS DROP WHEN YOU KILL THEM?",
+                                 "WHAT ORE IS NECESSARY TO ENCHANT ITEMS?",
+                                 "WHAT MATERIAL CAN YOU NOT MAKE ARMOR OUT OF?",
+                                 "WHAT MOB WILL ATTACK YOU IF YOU LOOK DIRECTLY INTO IT'S EYES?",
+                                 "HOW MANY DIFFERENT MODES CAN YOU PLAY IN MINECRAFT?",
+                                 "HOW MANY DIFFEENT BIOMES EXIST IN MINECRAFT?",
+                                 "HOW MANY EYES OF ENDER DO YOU NEED TO ACTIVATE AN END PORTAL?",
+                                 "WHAT ARMOR TYPE HAS THE LEAST AMOUNT OF DURABILITY?",
+                                 "WHAT MOB WILL EXPLODE IF YOU GET CLOSE TO IT?"];
+	window.correctAnswer = [ "D", "B", "C", "A", "E", "C", "E", "D", "B", "A" ];4 
+	window.answerA = [ 			"ALEX",
+                                "LEATHER",
+                                "REDSTONE",
+                                "STONE",
+                                "SKELETON",
+                                "3",
+                                "20",
+                                "4",
+                                "DIAMOND",
+                                "CREEPER"];
+	window.answerB = [          "GARY",
+								"BONES",
+								"DAIMONDS", 
+								"LEATHER",
+								"CREEPER",
+								"4",
+								"30",
+								"20",
+								"LEATHER",
+								"ENDERMAN "];
+	window.answerC = [ 			"HUGH",
+								"RAW MEAT",
+								"LAPIS LAZULI",
+								"GOLD",
+								"ZOMBIE",
+								"5",
+								"40",
+								"8",
+								"GOLD",
+								"ZOMBIE PIGMAN"];
+	window.answerD = [ 			"STEVE",  
+								"GUNPOWDWER",
+								"GOLD",
+								"IRON",
+								"SLIME",
+       							"6",
+								"50",
+								"12",
+								"IRON",
+								"PILLAGER"];
+	window.answerE = [ 			"John",
+								"Rotten Flesh",
+								"Emerald",
+								"DAIMOND",
+								"ENDERMAN",
+								"7",
+								"More than 60",
+								"10",
+								"CHAINMAIL",
+								"IRON GOLEM"];
+	window.response = [ 	    "STEVE IS THE SKIN YOU ARE WEARING WHEN YOU START MINECRAFT FOR THE FIRST TIME",
+								"SKELETONS ARE RANGED MOBS THAT USUALLY ATTACK FROM A DISTANCE, AND DROP BONES WHEN YOU KILL THEM",
+								"LAPIS LAZULI IS NEEDED TO ENCHANT EVERYTHING IN THE GAME, AND WITHOUT IT YOU CAN'T ENCHANT",
+								"ARMOR CAN ONLY BE MADE FROM LEATHER, IRON, GOLD, AND DIAMONDS",
+								"AN ENDERMAN WILL ONLY ATTACK YOU IF YOU LOOK DIRECTLY INTO IT'S EYES OR ATTACK IT FIRST",
+								"THE MODES ARE SURVIVAL, CREATIVE, ADVENTURE, SPECTATOR, AND HARDCORE",
+								"THERE ARE MORE THAN 60 DIFFERENT BIOMES IN MINECRAFT, BUT CAN BE CATEGORIZED INTO 5 GROUPS",
+								"THE END PORTAL WILL ONLY ACTIVATE ONE YOU PLACE AN EYE OF ENDER INTO EACH OF THE 12 FRAMES",
+								"LEATHER ARMOR HAS THE LEAST DURABILITY IN THE GAME, WITH DIAMOND HAVING THE MOST",
+								"CREEPERS ARE EXPLOSIVE MOBS THAT CHASE YOU WHEN YOU GET CLOSE ENOUGH"];
+							
+}
+						
